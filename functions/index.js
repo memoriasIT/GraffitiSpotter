57de3e68c93dd9ce7ffe2bb4e611f1bfb8745092
memoriasIT <memoriasdeuninformatico2017@gmail.com>
2020-11-19 21:19:10 +0100

    Initial project should be working now

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,20 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+// Used to get access to the database
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// Simple hello world to test the app
 exports.helloWorld = functions.https.onRequest((request, response) => {
    functions.logger.info("Hello logs!", {structuredData: true});
    response.send("Hello from Firebase!");
  });
+
+
+
+//  exports.getUsers = functions.https.onRequest((req, res) => {
+//     admin.firestore().collection('usuarios').get()
+//       let users = [];    
+//     data.forEach = [];
+//     })
+//  })
