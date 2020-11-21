c4041729ba7c41761bdb3d5325e567cff409c1d1
Roboron3042 <robertoms258@gmail.com>
2020-11-21 19:14:07 +0100

    Graffitis CRUD

--- functions/index.js
+++ functions/index.js
@@ -192,6 +192,116 @@ app.delete('/deleteComment', (req, res) => {
   })
 })
 
+/* 
+ * Graffitis 
+ */
+
+
+ // CREATE a new grafitti with a POST request. Takes input from body in JSON format.
+ app.post('/createGraffiti', (req, res) => {
+  // Take data from request body
+  const newGraffiti = {
+    autor: req.body.autor,
+    commentCount: req.body.commentCount,
+    descripcion: req.body.descripcion,
+    estado: req.body.estado,
+    fecha: req.body.fecha,
+    imagen: req.body.imagen,
+    likeCount: req.body.likeCount,
+    localizacion: req.body.localizacion,
+    tematica: req.body.tematica,
+    titulo: req.body.titulo,
+  }
+
+  // Add a document to the collection, use object newGraffiti
+  admin.firestore().collection('graffitis').add(newGraffiti)
+  .then (doc => {
+     res.json({message: 'Graffiti created successfully'});
+     return res;
+  })
+  .catch(err => {
+    res.status(500).json({error: 'Something went wrong'});
+    console.error(err);
+  })
+})
+
+
+
+// READ all graffitis
+app.get('/graffitis', (req, res) => {
+  admin
+  .firestore()
+  .collection('graffitis')
+  .orderBy('fecha')
+  .get()
+  .then((data) => {
+    let graffitis = [];
+    data.forEach((doc) => {
+      // Push an object to response with all graffiti parameters
+      graffitis.push({
+        id: doc.id,
+        body: doc.data().body,
+        autor: doc.data().autor.id,
+        commentCount: doc.data().commentCount,
+        descripcion: doc.data().descripcion,
+        estado: doc.data().estado,
+        fecha: doc.data().fecha,
+        imagen: doc.data().imagen,
+        likeCount: doc.data().likeCount,
+        localizacion: doc.data().localizacion,
+        tematica: doc.data().tematica,
+        titulo: doc.data().titulo,
+      });
+    });
+    
+    // Give response in JSON format
+    return res.json(graffitis);
+  })
+  .catch(err => console.error(err));
+});
+
+// UPDATE an existing graffiti given a id
+app.put('/updateGraffiti', (req, res) => {
+  // Take data from request body
+  const updateGraffiti = {
+    autor: req.body.autor,
+    commentCount: req.body.commentCount,
+    descripcion: req.body.descripcion,
+    estado: req.body.estado,
+    fecha: req.body.fecha,
+    imagen: req.body.imagen,
+    likeCount: req.body.likeCount,
+    localizacion: req.body.localizacion,
+    tematica: req.body.tematica,
+    titulo: req.body.titulo,
+  }
+
+  // Add a document to the collection with id of the graffiti, use object updateGraffiti
+  admin.firestore().collection('graffitis').doc(req.body.id).update(updateGraffiti)
+  .then (doc => {
+     res.json({message: 'Graffiti updated successfully'});
+     return res;
+  })
+  .catch(err => {
+    res.status(500).json({error: 'Something went wrong'});
+    console.error(err);
+  })
+})
+
+// DELETE an existing graffiti given a id
+app.delete('/deleteGraffiti', (req, res) => {
+  // Deletes a document with the id provided in body request
+  admin.firestore().collection('graffitis').doc(req.body.id).delete()
+  .then (doc => {
+     res.json({message: 'Graffiti deleted successfully'});
+     return res;
+  })
+  .catch(err => {
+    res.status(500).json({error: 'Something went wrong'});
+    console.error(err);
+  })
+})
+
 
 // https://url.com/api/....
 exports.api = functions.https.onRequest(app);
