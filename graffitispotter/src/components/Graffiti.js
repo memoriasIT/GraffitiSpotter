681c4faaaac6df71c26b3611c48a8d088489da5b
Roboron3042 <robertoms258@gmail.com>
2020-11-28 18:30:55 +0100

    Implementación inicial de lista de graffitis

---
+++ graffitispotter/src/components/Graffiti.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+import withStyles from '@material-ui/core/styles/withStyles';
+import { Link } from 'react-router-dom';
+//import dayjs from 'dayjs'
+//import relativeTime from 'dayjs/plugin/relativeTime'
+//Falta rehacer la fecha para usar lo que está comentado
+
+//MUI
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typorgraphy from '@material-ui/core/Typography';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+    card: {
+        display: 'flex',
+        marginBottom: 20
+    },
+    image: {
+        minWidth: 200
+    },
+    content: {
+        padding: 25,
+        objectFit: 'cover'
+    }
+}
+
+class Graffiti extends Component {
+    render(){
+        //dayjs.extend(relativeTime)
+        const {
+            classes,
+            graffiti : {
+                titulo,
+                fecha,
+                imagen,
+                id,
+                likeCount,
+                descripcion,
+                estado,
+                tematica,
+                localizacion,
+                commentCount,
+                autor
+            }
+        } = this.props
+        return(
+            <Card className={classes.card}>
+                <CardMedia
+                image={imagen}
+                title={titulo} 
+                className={classes.image}/>
+                <CardContent className={classes.content}>
+                    <Typorgraphy 
+                        variant="h5" 
+                        component={Link} 
+                        to={`/graffitis/${id}`}
+                    >
+                    {titulo}
+                    </Typorgraphy>
+                    <Typography variant="body2" color="textSecondary">{
+                        Date(fecha.seconds * 1000)
+                        //dayjs(createdAt).fromNow()
+                    }</Typography>
+                    <Typography variant="body1">{descripcion}</Typography>
+                </CardContent>
+            </Card>
+        )
+    }
+}
+
+export default withStyles(styles)(Graffiti);
--- graffitispotter/src/pages/home.js
+++ graffitispotter/src/pages/home.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+import Graffiti from '../components/Graffiti';
 
 class home extends Component {
+    state = {
+        graffitis: null
+    }
+    componentDidMount(){
+        axios.get('/graffitis')
+            .then(res => {
+                console.log(res.data)
+                this.setState({
+                    graffitis: res.data
+                })
+            })
+        .catch(err => console.log(err));
+    }
     render() {
+        let recentGraffitisMarkup = this.state.graffitis ? (
+            this.state.graffitis.map((graffiti) => 
+                <Graffiti key={graffiti.id} graffiti={graffiti}/> 
+                //<p>{graffiti.titulo}</p>
+            )) : <p>Loading...</p>
         return (
             <div>
                 <h1>Home Page</h1>
@@ -16,9 +37,19 @@ class home extends Component {
                 <br></br>
                 <Link to='/deleteUser'>deleteUser</Link>
                 // TODO UPDATE USER PAGE
+            <Grid container spaceing={16}>
+                <Grid item sm={8} xs={12}> 
+                    {recentGraffitisMarkup}
+                </Grid>
+            </Grid>
             </div>
-        )
+        );
     }
 }
+
+
+                //<Grid item sm={4} xs={12}>
+                //    Profile...
+                //</Grid>
 
 export default home
