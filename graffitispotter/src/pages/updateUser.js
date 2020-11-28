90aee97d00f5b66185e30a6c8f4ed6cd0c1a46ac
memoriasIT <memoriasdeuninformatico2017@gmail.com>
2020-11-28 23:59:19 +0100

    updateUser is now async, STILL DOESNT WORK BUT CLOSER??

--- graffitispotter/src/pages/updateUser.js
+++ graffitispotter/src/pages/updateUser.js
@@ -6,20 +6,65 @@ class updateUser extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoaded: false,
-      biografia: "",
+      isLoaded: null,
+      biografia: "a",
       edad: 0,
-      imagen: "",
-      nombre: "",
-      password: "",
-      username: ""
+      imagen: "b",
+      nombre: "c",
+      password: 'd',
+      username: 'e'
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  // componentDidMount() {
+  //   // Get access token from cookie NEEDS LOGIN OR REGISTER
+  //   const cookies = new Cookies();
+  //   var Bearer = 'Bearer ' + cookies.get('access-token');
+
+  //   var config = {
+  //       method: 'get',
+  //       url: 'http://localhost:5000/thegraffitispotter/us-central1/api/user/',
+  //       headers: { 
+  //           'Content-Type': 'application/json',
+  //           'Accept':'application/json',
+  //           'Authorization': Bearer
+  //           },
+  //       data : this.state.value
+  //   };
+    
+  //       axios(config)
+  //       .then(response => {
+  //           console.log("ComponentDidMount");
+  //           var credentials = response.data.credentials;
+  //           console.log(JSON.stringify(credentials));
+  //           this.setState({
+  //               biografia: credentials.biografia,
+  //               edad: credentials.edad,
+  //               imagen: credentials.imagen,
+  //               nombre: credentials.nombre,
+  //               password: credentials.password,
+  //               username: credentials.username,
+
+  //           });
+  //       })
+  //       .then( () => {
+  //         this.setState({
+  //           isLoaded : true
+  //         });
+  //       })
+  //       .catch(function (error) {
+  //           console.log(error);
+  //       });
+
+
+  // }
+
+
   componentDidMount() {
+
     // Get access token from cookie NEEDS LOGIN OR REGISTER
     const cookies = new Cookies();
     var Bearer = 'Bearer ' + cookies.get('access-token');
@@ -34,31 +79,32 @@ class updateUser extends Component {
             },
         data : this.state.value
     };
-    
-        axios(config)
-        .then(response => {
-            console.log("ComponentDidMount");
-            var credentials = response.data.credentials;
-            console.log(JSON.stringify(credentials));
+
+    this._asyncRequest = axios(config)
+          .then(response => {
+              console.log("ComponentDidMount");
+              var credentials = response.data.credentials;
+              console.log(JSON.stringify(credentials));
+              this.setState({
+                  biografia: credentials.biografia,
+                  edad: credentials.edad,
+                  imagen: credentials.imagen,
+                  nombre: credentials.nombre,
+                  password: credentials.password,
+                  username: credentials.username,
+  
+              });
+          })
+          .then( () => {
             this.setState({
-                biografia: credentials.biografia,
-                edad: credentials.edad,
-                imagen: credentials.imagen,
-                nombre: credentials.nombre,
-                password: credentials.password,
-                username: credentials.username,
-
+              isLoaded : true
             });
-        })
-        .then( () => {
-          this.setState({
-            isLoaded : true
+            this._asyncRequest = null;
+          })
+          .catch(function (error) {
+              console.log(error);
           });
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-
+  
 
   }
 
@@ -106,75 +152,82 @@ class updateUser extends Component {
 
 
   render() {
-    const { isLoaded } = this.state;
     //- biografia: "",
     //-   edad: 0,
     //-   imagen: "",
     //-   nombre: "",
     //-   password: "",
     //-   username: ""
-    return (
-      <React.Fragment>
-      {isLoaded ? 
-           
-          <form onSubmit={this.handleSubmit}>
+
+    if (this.state.isLoaded === null) {
+      // Render loading state ...
+      return(<h3>Cargando...</h3>);
+    } else {
+      // Render real UI ...
+      return(
+        <React.Fragment>
+      <form onSubmit={this.handleSubmit}>
             
 
-          <label>
-            Username:
-            <input
-              name="username"            type="text"
-              checked={this.state.username}
-              onChange={this.handleInputChange} />
-          </label>
-          <br />
-          <label>
-            Nombre:
-            <input
-              name="nombre"            type="text"
-              checked={this.state.nombre}
-              onChange={this.handleInputChange} />
-          </label>
-          <br />
-          <label>
-            Biografia:
-            <textarea 
-              name="biografia"            type="text"
-              checked={this.state.biografia}
-              onChange={this.handleInputChange} />
-          </label>
-          <br />
-          <label>
-            Imagen:
-            <input
-              name="imagen"            type="text"
-              checked={this.state.imagen}
-              onChange={this.handleInputChange} />
-          </label>
-          <br />
-          <label>
-            Contraseña:
-            <input
-              name="password"            type="password"
-              checked={this.state.password}
-              onChange={this.handleInputChange} />
-          </label>
-          <br />
-          <label>
-            Edad:
-            <input
-              name="edad"            type="number"
-              value={this.state.edad}
-              onChange={this.handleInputChange} />
-          </label>
+        <label>
+          Username:
+          <input
+            name="username"            type="text"
+            checked={this.state.username}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Nombre:
+          <input
+            name="nombre"            type="text"
+            checked={this.state.nombre}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Biografia:
+          <textarea 
+            name="biografia"            type="text"
+            checked={this.state.biografia}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Imagen:
+          <input
+            name="imagen"            type="text"
+            checked={this.state.imagen}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Contraseña:
+          <input
+            name="password"            type="password"
+            checked={this.state.password}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Edad:
+          <input
+            name="edad"            type="number"
+            value={this.state.edad}
+            onChange={this.handleInputChange} />
+        </label>
 
-          <input type="submit" value="Submit" />
-        </form>
+        <input type="submit" value="Submit" />
+      </form>
+    
+
+    </React.Fragment>
       
-        : "Cargando..." }
+      
+        );
+    }
 
-      </React.Fragment>
-    );
+    
   }
   
 
