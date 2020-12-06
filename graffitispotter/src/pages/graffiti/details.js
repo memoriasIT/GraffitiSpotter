9aaec28b7fa91f0ddc940e47cdaa05014b1d967f
Lorenzo De Linares <lorenzo.linares@icloud.com>
2020-12-06 14:03:40 +0100

    Added comments (sort of)

--- graffitispotter/src/pages/graffiti/details.js
+++ graffitispotter/src/pages/graffiti/details.js
@@ -5,7 +5,8 @@ import GraffitiDetails from '../../components/graffiti/GraffitiDetails';
 
 class graffiti extends Component {
     state = {
-        graffiti: null
+        graffiti: null,
+        comments: null
     }
     componentDidMount(){
         const id = this.props.match.params.id;
@@ -17,19 +18,33 @@ class graffiti extends Component {
                 })
                 .catch(err => console.log(err));
             }).catch(err => console.log(err));
+        axios.get('/commentsByGraffiti/${id}')
+           .then(res => {
+            console.log(res.data)
+            this.setState({
+                comments: res.data
+            })
+            .catch(err => console.log(err));
+        }).catch(err => console.log(err));
 
     }
     render(){
         let GraffitiMarkup = this.state.graffiti ? (
                 <GraffitiDetails graffiti={this.state.graffiti} />
             ) : <p>Loading...</p>
-
+        let recentComments = this.state.comments ? (
+            this.state.comments.map((comment) =>
+                <p>{comment.comentario}</p>
+            )) : <p>Loading...</p>
         return (
+            <div>
             <Grid container spaceing={16}>
                 <Grid item sm={8} xs={12}> 
                     {GraffitiMarkup}
                 </Grid>
             </Grid>
+                {recentComments}
+            </div>
 
         )
     }
